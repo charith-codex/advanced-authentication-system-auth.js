58e960b6fde9b1cb19f7163500f40f2006df65fd
charith-codex <pvm.charith@gmail.com>
2025-06-23 18:03:16 +0530

    setup register form

--- actions/auth.actions.ts
+++ actions/auth.actions.ts
@@ -1,11 +1,24 @@
 "use server";
 
-import { LoginSchema } from "@/schemas";
+import { LoginSchema, RegisterSchema } from "@/schemas";
 import { z } from "zod";
 
+// login action
 export const login = async (values: z.infer<typeof LoginSchema>) => {
   // validate fields from backend
   const validateFields = LoginSchema.safeParse(values);
+
+  if (!validateFields.success) {
+    return { error: "Invalid fields!" };
+  }
+
+  return { success: "Email sent!" };
+};
+
+// register action
+export const register = async (values: z.infer<typeof RegisterSchema>) => {
+  // validate fields from backend
+  const validateFields = RegisterSchema.safeParse(values);
 
   if (!validateFields.success) {
     return { error: "Invalid fields!" };
